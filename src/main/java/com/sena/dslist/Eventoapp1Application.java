d170cc9c814dde9b21977cc8f021915037ba326c
Marcelo Sena Dev <marcelosenadev1@gmail.com>
2023-07-16 14:37:10 -0300

    Projeto criado

---
+++ src/main/java/com/sena/dslist/Eventoapp1Application.java
@@ -0,0 +1,13 @@
+package com.sena.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eventoapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eventoapp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/dslist/Eventoapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eventoapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
